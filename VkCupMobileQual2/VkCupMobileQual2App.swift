270c90dd77f99082d11f4077e429093a582cbbf6
Lev Poznyakov <levochkaaa@icloud.com>
2023-01-11 16:25:14 +0300

    Initial Commit

---
+++ VkCupMobileQual2/VkCupMobileQual2App.swift
@@ -0,0 +1,12 @@
+// VkCupMobileQual2App.swift
+
+import SwiftUI
+
+@main
+struct VkCupMobileQual2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
